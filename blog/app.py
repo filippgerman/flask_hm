e36b4f0c02261214da5744c19eb73a2e21f373f4
home <filippgerman32@gmail.com>
2021-04-29 23:17:51 +0300

    create repo

---
+++ blog/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/<string:mag>", methods=["GET", "POST"])
+def index(mag: str):
+    return f"This is a {request.method} request"
+
+
